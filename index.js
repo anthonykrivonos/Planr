917c43b5fc30c9d171770ac6f7fbcde4c958812e
Anthony Krivonos <abkrivonos@gmail.com>
2017-01-27 13:28:52 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var http = require('http');
+var as = require('async');
+var aws = require('aws-sdk');
 var Yelp = require('yelp');
+
+// constants
+var MAX_WIDTH  = 100;
+var MAX_HEIGHT = 100;
 
 var EAT = 0, VISIT = 1, SHOP = 2;
 
@@ -35,9 +41,10 @@ exports.handler = (event, context) => {
 
         switch(event.request.intent.name) {
           case "MakePlans":
-            day = getDayAndMonthAndHours()[0];
-            month = getDayAndMonthAndHours()[1];
-            hours = getDayAndMonthAndHours()[2];
+            day = getDayAndMonthAndHours().day;
+            month = getDayAndMonthAndHours().month;
+            hours = getDayAndMonthAndHours().hours;
+            activity = getActivity();
 
             var origin = "http://ip-api.com/json";
             var body = "";
@@ -63,7 +70,39 @@ exports.handler = (event, context) => {
                 try {
                     var parsedData = JSON.parse(rawData);
                     zip = parsedData.zip;
-                    console.log(parsedData);
+                    origin = "http://api.openweathermap.org/data/2.5/weather?q=" + zip + "&APPID=205283d9c9211b776d3580d5de5d6338";
+
+                    var yelp = new Yelp({
+                        consumer_key: 'v_iFSeLqG70_lDRbxAAT0Q',
+                        consumer_secret: '8GH_JJ1T-RmPLbRkPGprweW_8dI',
+                        token: 'od8WpXP21za837KIiIUstmsW_EGOln63',
+                        token_secret: 'iag6zw7HfDOiDiN3QcNttTTcfQ4'
+                    });
+
+                    yelp.search({term: activity, location: zip})
+                    .then(function (data) {
+                          console.log('yelp start check');
+                          var tries = 0, isClosed = true;
+
+                          b =  Math.floor(Math.random() * Object.keys(data.businesses).length);
+
+                          business_name = data.businesses[b].name;
+                          business_review = data.businesses[b].rating;
+                          business_location = data.businesses[b].location.city;
+                          isClosed = !data.businesses[b].is_closed;
+
+                          console.log(`Business name: ${business_name}`);
+                          console.log(`Business review: ${business_review}`);
+                          console.log(`Business location: ${business_location}`);
+                          context.succeed(
+                                 generateResponse(
+                                     buildSpeechletResponse(getDialogue(business_name, business_review, business_location, isClosed), true), {}
+                               )
+                         )
+                    })
+                    .catch(function (err) {
+                          console.error(err);
+                    });
                 } catch (e) {
                     console.log(e.message);
                 }
@@ -71,133 +110,14 @@ exports.handler = (event, context) => {
             }).on('error', (e) => {
                 console.log(`Got error: ${e.message}`);
             });
-
-            origin = "http://api.openweathermap.org/data/2.5/weather?q=" + zip + "&APPID=205283d9c9211b776d3580d5de5d6338";
-            body = "";
-            http.get(origin, (res) => {
-                var statusCode = res.statusCode;
-                var contentType = res.headers['content-type'];
-                var error;
-                if (statusCode !== 200) {
-                    error = new Error(`Request Failed.\n` + `Status Code: ${statusCode}`);
-                } else if (!/^application\/json/.test(contentType)) {
-                    error = new Error(`Invalid content-type.\n` + `Expected application/json but received ${contentType}`);
-                }
-                if (error) {
-                    console.log(error.message);
-                    // consume response data to free up memory
-                    res.resume();
-                    return;
-                }
-            res.setEncoding('utf8');
-            var rawData = '';
-            res.on('data', (chunk) => rawData += chunk);
-            res.on('end', () => {
-                try {
-                    var parsedData = JSON.parse(rawData);
-                    weather = parsedData.weather[0].id;
-                    console.log(parsedData);
-                    setTimeout(function() {
-                        if (weather >= 200 && weather < 800)
-                            weather = true;
-                        else
-                            weather = false;
-                    }, 500);
-                } catch (e) {
-                    console.log(e.message);
-                }
-            });
-            }).on('error', (e) => {
-                console.log(`Got error: ${e.message}`);
-            });
-
-            activity = getActivity();
-            //origin = "https://api.yelp.com/v2/search/?term=" + activity + "&location= " + zip + "&limit=20&radius_filter=8000";
-            body = "";
-
-            var yelp = new Yelp({
-                consumer_key: 'v_iFSeLqG70_lDRbxAAT0Q',
-                consumer_secret: '8GH_JJ1T-RmPLbRkPGprweW_8dI',
-                token: 'od8WpXP21za837KIiIUstmsW_EGOln63',
-                token_secret: 'iag6zw7HfDOiDiN3QcNttTTcfQ4'
-            });
-
-            yelp.search({term: activity, location: zip})
-            .then(function (data) {
-                  console.log(data);
-                  setTimeout(function() {
-                        var jsonString = JSON.stringify(data);
-                        data = JSON.parse(jsonString);
-                        var tries = 0, isClosed = true;
-
-                        b = getBusiness(data.businesses.length);
-
-                        business_name = data.businesses[b].name;
-                        business_review = data.businesses[b].rating;
-                        business_review = data.businesses[b].location.city;
-
-                        while (tries < 20)
-                        {
-                              if (!data.businesses[b].is_closed)
-                              {
-                                    b = getBusiness(data.businesses.length);
-                                    business_name = data.businesses[b].name;
-                                    business_review = data.businesses[b].rating;
-                                    business_location = data.businesses[b].location.city;
-                                    isClosed = false;
-                                    tries += 20;
-                              }
-                        tries++;
-                  }
-            }, 500);
-            })
-            .catch(function (err) {
-                  console.error(err);
-            });
-            /*
-            https.get(origin, (response) => {
-              response.on('data', (chunk) => { body += chunk })
-              response.on('end', () => {
-                setTimeout(function() {
-                    var parsedData = JSON.parse(body);
-                    console.log(parsedData);
-                    var tries = 0, isClosed = true;
-                    b = getBusiness(parsedData.businesses.length);
-
-                    business_name = parsedData.businesses[b].name;
-                    business_review = parsedData.businesses[b].rating;
-                    business_review = parsedData.businesses[b].location.city;
-
-                    while (tries < 20)
-                    {
-                        if (!parsedData.businesses[b].is_closed)
-                        {
-                            b = getBusiness(parsedData.businesses.length);
-                            business_name = parsedData.businesses[b].name;
-                            business_review = parsedData.businesses[b].rating;
-                            business_location = parsedData.businesses[b].location.city;
-                            isClosed = false;
-                            tries += 20;
-                        }
-                        tries++;
-                    }
-                }, 500);
-          })*/
-        })
-            setTimeout(function() {
-                context.succeed(
-                    generateResponse(
-                        buildSpeechletResponse(getDialogue(business_name, business_review, business_location, isClosed), true), {}
-                    )
-                );
-            }, 2000);
             break;
 
           default:
             throw "Invalid intent";
-        }
+            break;
 
-        break;
+      };
+      break;
 
       case "SessionEndedRequest":
         // Session Ended Request
@@ -206,7 +126,7 @@ exports.handler = (event, context) => {
 
       default:
         context.fail(`INVALID REQUEST TYPE: ${event.request.type}`)
-
+        break;
     }
 
   } catch(error) { context.fail(`Exception: ${error}`) }
@@ -238,13 +158,13 @@ generateResponse = (speechletResponse, sessionAttributes) => {
 
 function getDayAndMonthAndHours() {
     var date = new Date();
-    var hours = new Date().getHours();
+    var h = new Date().getHours();
 	date = date.toISOString();
 	date.split("-");
-	var month = date[1];
-	var day = date[2];
+	var m = parseInt(date[1]);
+	var d = parseInt(date[2]);
 	var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-	return [day, monthNames[month+1], hours];
+	return {day: d, month: monthNames[m+1], hours: h};
 }
 
 function getActivity() {
@@ -258,12 +178,8 @@ function getActivity() {
     return rand;
 }
 
-function getBusiness(length) {
-    return Math.floor((Math.random * length));
-}
-
 function getDialogue(name, review, location, closed) {
-    var rand = Math.floor((Math.random * 3) + 1);
+    var rand = Math.floor((Math.random() * 3) + 1);
     var str = "";
     switch (rand) {
         case 1:
@@ -282,6 +198,7 @@ function getDialogue(name, review, location, closed) {
             str += "I think you should go to " + name + " in " + location + ". It has a " + review + " Yelp review.";
             break;
         default:
+            str += "Try to visit " + name + " in " + location + ". It's Yelp rating is " + review + ".";
             break;
     }
     if (closed)
@@ -290,10 +207,10 @@ function getDialogue(name, review, location, closed) {
         switch (rand)
         {
             case 1:
-                str += " However, I think it's closed.";
+                str += " However, I think it's closed. Go sleep!";
                 break;
             default:
-                str += " But it may be closed at this hour. You should try tomorrow.";
+                str += " But it may be closed at this hour. You should try tomorrow. Talk to you then!";
                 break;
         }
     }
